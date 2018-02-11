69bd7bcf202f54fdc119a489e8fbdca3d97b0393
栗鑫 <lx389029980@outlook.com>
2018-02-11 23:50:50 +0800

    V1

--- TimerExample/Program.cs
+++ TimerExample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace TimerExample
 {
@@ -10,7 +11,14 @@ namespace TimerExample
     {
         static void Main(string[] args)
         {
+            Timer myTimer = new Timer(2000);
 
+            myTimer.Elapsed += MyTimer_Elapsed;
+        }
+
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            
         }
     }
 }
