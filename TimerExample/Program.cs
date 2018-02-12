ede2d872a70c1331a0ef7c6279bea1c2305f04ca
栗鑫 <lx389029980@outlook.com>
2018-02-12 14:15:29 +0800

    Complete!

--- TimerExample/Program.cs
+++ TimerExample/Program.cs
@@ -12,13 +12,33 @@ namespace TimerExample
         static void Main(string[] args)
         {
             Timer myTimer = new Timer(2000);
+            myTimer.Elapsed += MyTimer_Elapsed;      
+            myTimer.Start();
 
-            myTimer.Elapsed += MyTimer_Elapsed;
+            Timer myTimer1 = new Timer(1000);
+            myTimer1.Elapsed += MyTimer_Elapsed1;
+            myTimer1.Start();
+
+            Console.WriteLine("Press Entre to remove the yellow event.");
+            Console.ReadLine();
+
+            myTimer.Elapsed -= MyTimer_Elapsed;
+
+            Console.ReadLine();
+        }
+
+        private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Elapsed1: {0:HH:mm:ss.fff}", e.SignalTime);
         }
 
         private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}" ,e.SignalTime);
         }
     }
 }
